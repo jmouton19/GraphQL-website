256c20c0ad937dc65abd3eb3d4bc4f5cb284624c
JC Mouton <22549889@sun.ac.za>
2022-04-09 00:23:37 +0200

    parallel query fix

--- webAPI/graphQL/Query.cs
+++ webAPI/graphQL/Query.cs
@@ -4,28 +4,28 @@ using webAPI.Models;
 
 namespace webAPI.graphQL{
     public class Query{
-        [UseProjection]
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<User> GetUsers([ScopedService] AppDbContext context){
             return context.Users;
         }
+      [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
-        [UseDbContext(typeof(AppDbContext))]
         public IQueryable<Group> GetGroups([ScopedService] AppDbContext context){
             return context.Groups;
         }
+       [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
-        [UseDbContext(typeof(AppDbContext))]
         public IQueryable<Membership> GetMemberships([ScopedService] AppDbContext context){
             return context.Memberships;
         }
-        [UseProjection]
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<Post> GetPosts([ScopedService] AppDbContext context){
             return context.Posts;
         }
-        [UseProjection]
         [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
         public IQueryable<Comment> GetComments([ScopedService] AppDbContext context){
             return context.Comments;
         }
