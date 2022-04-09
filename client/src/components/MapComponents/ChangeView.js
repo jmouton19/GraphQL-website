51e2e12a8d06e430c404294a66630b4cae292723
Phillip <schommarzp@gmail.com>
2022-04-09 14:03:13 +0200

    Centering maps and pins

--- client/src/components/MapComponents/ChangeView.js
+++ client/src/components/MapComponents/ChangeView.js
@@ -1,29 +1,8 @@
-import React from 'react';
 import { useMap } from 'react-leaflet';
-import { Fab } from '@mui/material';
-import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
 
 function ChangeView({ center, zoom }) {
   const map = useMap();
   map.flyTo(center, zoom, { duration: 1 });
-  return (
-    <>
-      <Fab
-        style={{
-          margin: 0,
-          top: 'auto',
-          right: 20,
-          bottom: 20,
-          left: 'auto',
-          position: 'fixed',
-        }}
-        color="primary"
-        aria-label="edit"
-        onClick={() => map.flyTo(center, zoom, { duration: 1 })}
-      >
-        <CenterFocusStrongIcon />
-      </Fab>
-    </>
-  );
+  return null;
 }
 export default ChangeView;
--- client/src/pages/MapPage.js
+++ client/src/pages/MapPage.js
@@ -2,20 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import cheeseMarker from '../assets/cheese-pin.png';
+import userMarker from '../assets/userMarker.png'
 import { Icon } from 'leaflet';
 import ChangeView from '../components/MapComponents/ChangeView';
 import PostSlider from '../components/MapComponents/PostSlider';
-import { Container } from '@mui/material';
+import { Container, Fab } from '@mui/material';
 import { usePosts } from '../providers/PostProvider';
+
+import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
 
 const cheeseIcon = new Icon({
   iconUrl: cheeseMarker,
   iconSize: [32, 46],
 });
 
+const userIcon = new Icon({
+  iconUrl: userMarker,
+  iconSize: [32, 32],
+})
+
 function MapPage() {
   const [userLocation, setUserLocation] = useState([-33.9321, 18.8602]);
   const [focusedPost, setFocusedPost] = useState(null);
+  const [center, setCenter] = useState([-33.9321, 18.8602])
   const posts = usePosts();
 
   useEffect(() => {
@@ -24,19 +33,24 @@ function MapPage() {
     });
   }, []);
 
+  useEffect(() => {
+    setCenter(userLocation);
+  }, [userLocation])
+  
+
   return (
     <>
       <MapContainer
-        center={userLocation}
+        center={center}
         zoom={16}
         style={{ height: '40vh', marginTop: '65px' }}
       >
-        <ChangeView center={userLocation} zoom={16} />
+        <ChangeView center={center} zoom={16} />
         <TileLayer
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={userLocation} icon={cheeseIcon}>
+        <Marker position={userLocation} icon={userIcon}>
           <Popup>
             You are Gru-<b>hère</b>.
           </Popup>
@@ -48,6 +62,7 @@ function MapPage() {
             eventHandlers={{
               click: (e) => {
                 setFocusedPost(post);
+                setCenter(post.location);
               },
             }}
           />
@@ -56,6 +71,23 @@ function MapPage() {
       <Container flexGrow>
         <PostSlider posts={posts} focusedPost={focusedPost} />
       </Container>
+      <Fab
+        style={{
+          margin: 0,
+          top: 'auto',
+          right: 20,
+          bottom: 20,
+          left: 'auto',
+          position: 'fixed',
+        }}
+        color="primary"
+        aria-label="edit"
+        onClick={() => {
+          setCenter([...userLocation]);
+        }}
+      >
+        <CenterFocusStrongIcon />
+      </Fab>
     </>
   );
 }
