3e6dfd2a01e7bddd6ef2b624c0a97d2dd0b6e694
JC Mouton <22549889@sun.ac.za>
2022-04-13 01:55:48 +0200

    add filtering and sorting

--- webAPI/graphQL/Query.cs
+++ webAPI/graphQL/Query.cs
@@ -1,31 +1,42 @@
 using webAPI.data;
 using webAPI.Models;
-
+using UseFilteringAttribute = HotChocolate.Data.UseFilteringAttribute;
+using UseSortingAttribute = HotChocolate.Data.UseSortingAttribute;
 
 namespace webAPI.graphQL{
     public class Query{
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<User> GetUsers([ScopedService] AppDbContext context){
             return context.Users;
         }
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Group> GetGroups([ScopedService] AppDbContext context){
             return context.Groups;
         }
        [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Membership> GetMemberships([ScopedService] AppDbContext context){
             return context.Memberships;
         }
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Post> GetPosts([ScopedService] AppDbContext context){
             return context.Posts;
         }
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
+        [UseFiltering]
+        [UseSorting]
         public IQueryable<Comment> GetComments([ScopedService] AppDbContext context){
             return context.Comments;
         }
