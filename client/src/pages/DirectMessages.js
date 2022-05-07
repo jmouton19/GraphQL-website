37e7f54a86e1590c96be26458800f8d3842b9a47
Phillip <schommarzp@gmail.com>
2022-05-07 12:03:55 +0200

    I am so confused

--- client/src/pages/DirectMessages.js
+++ client/src/pages/DirectMessages.js
@@ -9,40 +9,80 @@ import {
   OutlinedInput,
   Stack,
   Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  Avatar,
+  ListItemAvatar,
+  ListItemText,
 } from '@mui/material';
+import { useApolloClient, gql } from '@apollo/client';
 
 const gun = Gun({
   peers: ['http://localhost:3030/gun'],
 });
 
 const initialState = {
-  messages: [],
+  messages: [{}],
 };
 
 function reducer(state, message) {
-  return {
-    messages: [...state.messages, message],
-  };
+  switch (message.action) {
+    case 'ADD':
+      return {
+        messages: [message, ...state.messages],
+      };
+    case 'RESET':
+      return {
+        messages: [{}],
+      };
+  }
 }
 
 export default function DirectMessages() {
+  const client = useApolloClient();
   const authUser = useAuthUser();
   const [message, setMessage] = useState('');
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [friends, setFriends] = useState([]);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+  const [node, setNode] = useState('');
 
   useEffect(() => {
-    const stream = gun.get('messages');
-    stream.map().on((m) => {
-      dispatch({
-        name: m.name,
-        message: m.message,
-        createdAt: m.createdAt,
-      });
-    });
+    getFriends();
   }, []);
 
+  useEffect(() => {
+    if (node !== '') {
+      const stream = gun.get(node);
+      stream.map().once((m) => {
+        if (
+          state.messages[0].name !== m.name &&
+          state.messages[0].message !== m.message
+        ) {
+          dispatch({
+            name: m.name,
+            message: m.message,
+            createdAt: m.createdAt,
+            action: 'ADD',
+          });
+        }
+      });
+    }
+  }, [node]);
+
+  const startStream = (friend) => {
+    setSelectedFriend(friend);
+    dispatch({ action: 'RESET' });
+    if (authUser.username.localeCompare(friend.username) == -1) {
+      setNode(authUser.username.concat(friend.username));
+    } else {
+      setNode(friend.username.concat(authUser.username));
+    }
+  };
+
   function saveMessage() {
-    const messages = gun.get('messages');
+    const messages = gun.get(node);
     const date = new Date();
     messages.set({
       name: authUser.username,
@@ -51,11 +91,64 @@ export default function DirectMessages() {
     });
     setMessage('');
   }
+
+  const getFriends = () => {
+    client
+      .query({
+        fetchPolicy: 'no-cache',
+        query: gql`
+        query{
+          sent:friendships(where: {and:[{ accepted: { eq: true } },{sender:{id:{eq:${authUser.id}}}}]})
+          {
+              receiver{
+                  avatar
+                  lastName
+              firstName
+                  id
+                  username
+              }
+          }
+           received:   friendships(where: {and:[{ accepted: { eq: true } },{receiver:{id:{eq:${authUser.id}}}}]})
+          {
+              sender{
+                  avatar
+                  lastName
+                  firstName
+                  id
+                  username
+              }
+          }
+      }
+    `,
+      })
+      .then((result) => {
+        setFriends([
+          ...result.data.sent.map((a) => a.receiver),
+          ...result.data.received.map((a) => a.sender),
+        ]);
+      });
+  };
+
   return (
     <>
       <Grid container padding={2}>
         <Grid item xs={4}>
-          Friends
+          <List>
+            {friends.map((friend) => (
+              <ListItemButton key={friend.id}>
+                <ListItem onClick={() => startStream(friend)}>
+                  <ListItemAvatar>
+                    <Avatar src={friend.avatar} />
+                  </ListItemAvatar>
+
+                  <ListItemText
+                    primary={friend.username}
+                    secondary={`${friend.firstName} ${friend.lastName}`}
+                  />
+                </ListItem>
+              </ListItemButton>
+            ))}
+          </List>
         </Grid>
         <Grid item xs={8}>
           <Stack>
@@ -74,7 +167,13 @@ export default function DirectMessages() {
             </Stack>
             <Stack>
               {state.messages.map((msg) => (
-                <Typography>{msg.message}</Typography>
+                <div>
+                  {msg.name !== undefined && (
+                    <Typography
+                      key={msg.indexOf}
+                    >{`${msg.name}: ${msg.message}`}</Typography>
+                  )}
+                </div>
               ))}
             </Stack>
           </Stack>
